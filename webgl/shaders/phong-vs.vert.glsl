67cedaa5004951e1696909c59b535386066ff17f
IndianBoy42 <rohanmed123@gmail.com>
2020-11-18 16:46:05 +0800

    move shaders out of webgl

--- webgl/shaders/phong-vs.vert.glsl
+++
@@ -1,8 +0,0 @@
-varying vec3 vNormal;
-varying vec3 vPos;
-void main(){
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    vPos = (modelViewMatrix * vec4(position, 1.0)).xyz;
-    vNormal = normalMatrix * normal;
-    gl_Position.z += 0.0001; //for wireframe z-fighting
-}
