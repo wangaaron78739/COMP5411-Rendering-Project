56217a4a49f35a4c1db2452128293a6bdd9bf93b
wangaaron78739 <aswang@connect.ust.hk>
2020-12-01 20:45:00 +0800

    reverse distance again

--- webgl/cfg.js
+++ webgl/cfg.js
@@ -36,7 +36,7 @@ const defaultCfg =
     addLens: function () {
         let minD = this.lensesOptions.length ? (this.lensesOptions[this.lensesOptions.length - 1].lensPosition.z + 1) : 1;
         this.lensesOptions.push({
-            lensPosition: new THREE.Vector3(Math.random() * 10.0 - 5.0, Math.random() * 10.0 - 5.0, - Math.floor(Math.random() * 10.0) + minD),
+            lensPosition: new THREE.Vector3(Math.random() * 10.0 - 5.0, Math.random() * 10.0 - 5.0, Math.floor(Math.random() * 10.0) + minD),
             lensRadius1: 1000.0,
             lensRadius2: 1000.0,
             lensWidth: 0.5,
@@ -54,13 +54,15 @@ function addLensesToWorld(cfg, world) {
     world.lenses.forEach(lens => delete lens);
     world.lenses = [];
     cfg.lensesOptions.forEach((config, idx) => {
+        const newPos = config.lensPosition.clone();
+        newPos.z = -config.lensPosition.z;
         const lens = new THREE.Mesh(new THREE.CircleGeometry(1, 32), new THREE.ShaderMaterial({
             uniforms: {
                 lensRadius1: { value: config.lensRadius1 },
                 lensRadius2: { value: config.lensRadius2 },
                 lensDiameter: { value: config.lensDiameter },
                 lensWidth: { value: config.lensWidth },
-                lensPosition: { value: config.lensPosition },
+                lensPosition: { value: newPos },
                 screen: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
                 eta: { value: [1.15, 1.17, 1.19, 1.21, 1.23, 1.25] }
             },
@@ -68,7 +70,7 @@ function addLensesToWorld(cfg, world) {
             vertexShader: getShaderCustom('lens', 'vs'),
             fragmentShader: getShaderCustom('lens', 'ps'),
         }));
-        lens.position.set(config.lensPosition.x, config.lensPosition.y, config.lensPosition.z);
+        lens.position.copy(newPos);
         lens.scale.set(config.lensDiameter, config.lensDiameter);
         lens.idx = idx;
         world.lenses.push(lens);
